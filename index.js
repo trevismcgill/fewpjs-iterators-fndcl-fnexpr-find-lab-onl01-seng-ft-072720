ce9f74a6322f82f2c130cca87e2ea8f1b378723d
trevismcgill <trevismcgill25@gmail.com>
2020-11-08 22:25:14 -0600

    commit tree 478b89f29e80ecde49bbcbb67ebdffceb94bee50

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+    let game = array.find(g => g.result === "W")
+    debugger
+    return !!game ? game.year : undefined
+};
